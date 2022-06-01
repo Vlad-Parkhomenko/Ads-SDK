a2b3368d06a47a27a00adefcca68594718cfd147
vlad_parkhomenko <vlad@visualestate.com>
2022-06-01 18:20:08 +0300

    set up sending request for media loading

---
+++ Assets/Scripts/Media/MediaManager.cs
@@ -0,0 +1,25 @@
+﻿using AdsSdk.Networking;
+using UnityEngine;
+
+namespace AdsSdk.Media
+{
+    public class MediaManager : MonoBehaviour
+    {
+        private NetworkAPI _api = new NetworkAPI();
+
+        private void Start()
+        {
+            LoadMediaData();
+        }
+
+        private void LoadMediaData()
+        {
+            _api.LoadMediaData(HandleMediaLoading);
+        }
+
+        private void HandleMediaLoading(string xml)
+        {
+            Debug.Log(xml);
+        }
+    }
+}
