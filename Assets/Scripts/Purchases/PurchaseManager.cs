87cfb45216570440a9d63aa9786d246bc795b0a6
vlad_parkhomenko <vlad@visualestate.com>
2022-06-02 02:00:55 +0300

    set up purchases

---
+++ Assets/Scripts/Purchases/PurchaseManager.cs
@@ -0,0 +1,24 @@
+﻿using AdsSdk.Networking;
+using UnityEngine;
+
+namespace AdsSdk.Purchases
+{
+    public class PurchaseManager : MonoBehaviour
+    {
+        [SerializeField] private GameObject _productInfoPanel;
+
+        private NetworkAPI _api = new NetworkAPI();
+
+        private void Start()
+        {
+            var obj = new { id = 1 };
+            string json = JsonUtility.ToJson(obj);
+            _api.SendPurchaseRequest(json, HandlePurchaseData);
+        }
+
+        private void HandlePurchaseData(string json)
+        {
+            Debug.Log(json);
+        }
+    }
+}
