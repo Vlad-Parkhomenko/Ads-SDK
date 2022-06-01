827235951db3ec3fab34d53046125902d0876b32
vlad_parkhomenko <vlad@visualestate.com>
2022-06-01 22:38:10 +0300

    set up xml deserialization and playing video

--- Assets/Scripts/Media/MediaManager.cs
+++ Assets/Scripts/Media/MediaManager.cs
@@ -1,10 +1,16 @@
-﻿using AdsSdk.Networking;
+﻿using AdsSdk.Media.Data;
+using AdsSdk.Networking;
+using System.IO;
+using System.Xml.Serialization;
 using UnityEngine;
+using UnityEngine.Video;
 
 namespace AdsSdk.Media
 {
     public class MediaManager : MonoBehaviour
     {
+        [SerializeField] private VideoPlayer _player;
+
         private NetworkAPI _api = new NetworkAPI();
 
         private void Start()
@@ -12,14 +18,30 @@ namespace AdsSdk.Media
             LoadMediaData();
         }
 
-        private void LoadMediaData()
+        public void PlayVideo()
         {
-            _api.LoadMediaData(HandleMediaLoading);
+            _player.Play();
         }
 
-        private void HandleMediaLoading(string xml)
+        private void LoadMediaData()
+        {
+            _api.LoadMediaData(RetrieveVideoURL);
+        }
+
+        private void RetrieveVideoURL(string xml)
         {
             Debug.Log(xml);
+
+            var serializer = new XmlSerializer(typeof(VAST));
+
+            VAST vast;
+            using (StringReader reader = new StringReader(xml))
+            {
+                vast = (VAST)serializer.Deserialize(reader);
+            }
+
+            _player.url = vast.Ad.InLine.Creatives.Creative.Linear.MediaFiles.MediaFile;
+            PlayVideo();
         }
     }
 }
