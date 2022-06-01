cfa953f458642bb77f146fdb6526244cd52ae37f
vlad_parkhomenko <vlad@visualestate.com>
2022-06-02 01:34:06 +0300

    added opening persistent data path

---
+++ Assets/Scripts/Editor/PersistentDataPath.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEditor;
+using UnityEngine;
+
+public class PersistentDataPath
+{
+    [MenuItem("Tools/Open persistent data path")]
+    private static void OpenPesritentDataPath()
+    {
+        Process.Start(Application.persistentDataPath);
+    }
+}
