41041cd19478c168921936009ce904c41c9e4694
vlad_parkhomenko <vlad@visualestate.com>
2022-06-01 16:21:31 +0300

    set up requests creator

---
+++ Assets/Scripts/Networking/Requests/RequestCreator.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+namespace AdsSdk.Networking.Requests
+{
+    public static class RequestCreator
+    {
+        public static UnityWebRequest CreateGetRequest(string url)
+        {
+            var request = UnityWebRequest.Get(url);
+
+            return request;
+        }
+
+        public static UnityWebRequest CreatePostRequest(string url, string body)
+        {
+            var request = UnityWebRequest.Post(url, body);
+
+            return request;
+        }
+    }
+}
