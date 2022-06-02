c6f1603dd8f103e1e39f8cb5ea8aabe5b876e35c
vlad_parkhomenko <vlad@visualestate.com>
2022-06-02 18:43:01 +0300

    added missing changes

---
+++ Assets/Scripts/Purchases/Data/PaymentData.cs
@@ -0,0 +1,25 @@
+﻿using LitJson;
+using System;
+
+namespace AdsSdk.Purchases.Data
+{
+    public struct PaymentData
+    {
+        public string Email { get; set; }
+        public string CardNumber { get; set; }
+        public DateTime ExpirationDate { get; set; }
+
+        public string ToJson()
+        {
+            var jsonData = new JsonData
+            {
+                ["email"] = Email,
+                ["card_number"] = CardNumber,
+                ["expiration_date"] = ExpirationDate.ToString("MM/yyyy")
+            };
+
+            string json = jsonData.ToJson();
+            return json;
+        }
+    }
+}
---
+++ Assets/Scripts/UI/PaymentView.cs
@@ -0,0 +1,36 @@
+﻿using AdsSdk.Purchases.Data;
+using System;
+using System.Globalization;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace AdsSdk.UI
+{
+    public class PaymentView : MonoBehaviour
+    {
+        [SerializeField] private InputField _email;
+        [SerializeField] private InputField _creditCardNumber;
+        [SerializeField] private InputField _cardExpirationDate;
+        [SerializeField] private Button _confirm;
+
+        public event Action<PaymentData> PurchaseDataCollected;
+
+        private void Awake()
+        {
+            _confirm.onClick.AddListener(CaptureData);
+        }
+
+        private void CaptureData()
+        {
+            var paymentData = new PaymentData
+            {
+                Email = _email.text,
+                CardNumber = _creditCardNumber.text,
+                ExpirationDate = DateTime.ParseExact(_cardExpirationDate.text, "MM/yyyy", CultureInfo.InvariantCulture)
+            };
+
+            PurchaseDataCollected?.Invoke(paymentData);
+        }
+    }
+
+}
